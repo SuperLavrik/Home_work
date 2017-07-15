6ea63c1a72ad93e43f821a70c09041e3b60c7576
SuperLavrik <superlavrik@gmail.com>
2017-07-15 13:22:09 -0700

    initail commit

---
+++ task_6.py
@@ -0,0 +1,12 @@
+import math
+
+a = 13
+b = 56
+c = 3
+
+print("При a = %d \fb = %d \fc = %d " %(a,b,c))
+result_task_6 = (math.log1p(c) / -b ) ** 4  + abs(a)
+print("Результат задания №6 %.2f" %(result_task_6))
+
+
+
