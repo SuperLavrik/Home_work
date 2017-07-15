a11a0def10fb71afe4f3bae9ffcbaa9676cb06ac
SuperLavrik <superlavrik@gmail.com>
2017-07-15 16:18:42 -0700

    corrected remark 1

--- task_2.py
+++ task_2.py
@@ -3,7 +3,7 @@ b = 9
 
 print("При a = %d \fb = %d " %(a,b))
 result_task_2 = (a**2 +  b**2) %2
-print("Результат задания №2 %d" %(result_task_2))
+print("Результат задания №2 %d" % result_task_2)
 
 
 
