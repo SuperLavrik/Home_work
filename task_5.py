19383070a73fe4e582635227baaa4e5296d75c7d
SuperLavrik <superlavrik@gmail.com>
2017-07-15 13:19:50 -0700

    initail commit

---
+++ task_5.py
@@ -0,0 +1,12 @@
+import math
+
+a = 13
+b = 12
+c = 7
+
+print("При a = %d \fb = %d \fc = %d " %(a,b,c))
+result_task_5 = math.fabs(a - b) / (a + b ) **3 - math.cos(c)
+print("Результат задания №5 %.2f" %(result_task_5))
+
+
+
