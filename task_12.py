3d072b9d71bb9c23e5565ebd0fbab11ddf2397b4
SuperLavrik <superlavrik@gmail.com>
2017-07-22 15:31:21 -0700

    remade code

--- task_12.py
+++ task_12.py
@@ -1,7 +1,9 @@
 def sum_of_digits(numerik):
-    string = str(numerik)
-    summa = int(string[0])+int(string[1])+int(string[2])
+    #string = str(numerik)
+    number_dec = (numerik-numerik//100 * 100 - numerik%10) / 10
+    summa = numerik//100 + number_dec + numerik%10
+    #+numerik//100int(string[2])
     return summa
 
-number = 739
+number = 138
 print("Сумма цифр числа %d равна %d" % (number, sum_of_digits(number)))
