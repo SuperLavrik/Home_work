cd5423029980fdc107d748e7a06035314d9284b4
SuperLavrik <superlavrik@gmail.com>
2017-07-22 15:39:44 -0700

    remade code

--- task_12.py
+++ task_12.py
@@ -1,8 +1,6 @@
 def sum_of_digits(numerik):
-    #string = str(numerik)
     number_dec = (numerik-numerik//100 * 100 - numerik%10) / 10
     summa = numerik//100 + number_dec + numerik%10
-    #+numerik//100int(string[2])
     return summa
 
 number = 138
