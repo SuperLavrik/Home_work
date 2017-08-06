d45e416ad5306b59876b348d48ba93a52e467ebf
SuperLavrik <superlavrik@gmail.com>
2017-08-06 04:26:42 -0700

    remode code for all range numbers

--- task_26.py
+++ task_26.py
@@ -1,19 +1,18 @@
-def is_simple(number):
-    if not number%2 and number//2 != 1 :
-        return False
-    elif not number%3 and number//3 != 1 :
-        return False
-    elif not number%5 and number//5 != 1 :
-        return False
-    elif not  number%7 and number//7 != 1:
-       return False
-    else:
-        return True
+def simple_lst(number,lst_simple):
+    for i in lst_simple[1:]:
+        if number == 1 or number % i == 0 or number == i:
+            return lst_simple
+    return lst_simple.append(number)
+
 
 lower_bound  = 1
 upper_bound = 100
+lst_simple = []
 print ("Simple numbers more %d and less %d :" %(lower_bound, upper_bound))
 
-for i in range(lower_bound, upper_bound):
-    if is_simple(i):
-        print (i)
+for  j in range(1, upper_bound +1):
+    simple_lst(j,lst_simple)
+
+for  k in lst_simple:                   # print simple number in preset range
+    if k >= lower_bound:
+        print (k)
