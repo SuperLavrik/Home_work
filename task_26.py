457b0a48f52cba5d6f3ebe5da8204e3308faaef4
SuperLavrik <superlavrik@gmail.com>
2017-08-08 14:52:29 -0700

    remode code

--- task_26.py
+++ task_26.py
@@ -1,9 +1,8 @@
-def simple_lst(number,lst_simple):
+def is_number_simple_in_lst(number,lst_simple):
     for i in lst_simple[1:]:
         if number == 1 or number % i == 0 or number == i:
-            return lst_simple
-    return lst_simple.append(number)
-
+            return False
+    return True
 
 lower_bound  = 1
 upper_bound = 100
@@ -11,7 +10,8 @@ lst_simple = []
 print ("Simple numbers more %d and less %d :" %(lower_bound, upper_bound))
 
 for  j in range(1, upper_bound +1):
-    simple_lst(j,lst_simple)
+    if is_number_simple_in_lst(j,lst_simple):
+        lst_simple.append(j)
 
 for  k in lst_simple:                   # print simple number in preset range
     if k >= lower_bound:
