c1e38f5f4e77e76c70920326dafa7e26e1132e41
SuperLavrik <superlavrik@gmail.com>
2017-08-15 15:25:06 -0700

    remode code

--- test_10.py
+++ test_10.py
@@ -8,13 +8,17 @@ upper_bound = 100
 
 matrix = [[random.randint(lower_bound, upper_bound) for j in range(number_column)]
           for i in range(number_string)]
+
 print (" Initial matrix:")
 for i in range (number_string):
     for j in range (number_column):
         print (matrix[i][j], end = "\t\t" )
     print()
+
+transp_matrix = [[matrix[i][j] for i in range (number_string)] for j in range (number_column)]
+
 print ("\n Transposed matrix:")
-for i in range (number_column):
-    for j in range (number_string):
-        print (matrix[j][i], end = "\t\t" )
+for k in range (number_column):
+    for l in range (number_string):
+        print (transp_matrix[k][l], end = "\t\t" )
     print()
