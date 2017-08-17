a1cb6e6ff98dfb3dc36a6c4eb9d870379b24eaf9
SuperLavrik <superlavrik@gmail.com>
2017-08-17 13:28:54 -0700

    fixed (i hope....)

--- task_29.py
+++ task_29.py
@@ -5,7 +5,9 @@ upper_bound = 1
 numb_elem = 11
 
 list = [random.randint(lower_bound,upper_bound) for x in range(numb_elem)]
+#list = [0,1,0,0,0,1,0,0,0,1,-1]
 list_count = [list.count(-1),list.count(0),list.count(1)]
+list_elem = [-1,0,1]
 
 print("Basic list:")
 print (list)
@@ -16,4 +18,5 @@ if list_count[0] == list_count[1] or list_count[0] == list_count[2] \
     pass
 else :
     print ("Element %d repeats %d times " % \
-           (list[list_count.index(max(list_count))], max(list_count)))
+           (list_elem[list_count.index(max(list_count))], max(list_count)))
+
