e3f6d04fb93968c800fc07a1d947468b852ca743
SuperLavrik <superlavrik@gmail.com>
2017-08-18 05:26:45 -0700

    remode code

--- test_4.py
+++ test_4.py
@@ -1,2 +1,15 @@
-x = str(input("Input five-digit number  - "))
-print ("Multiplication odd digit = ", int(x[0]) * int(x[2]) * int(x[4]))
+count_digit = 5
+multip_odd = 0
+
+print("Input %d-digit number  - " % count_digit, end="")
+x = str(input())
+
+for i in range(count_digit):
+    if i >= len(x):             # if input number less counn_digit
+        x +="1"
+    if   int(x[i])%2:
+        if not multip_odd:      #  when we have fist odd number
+            multip_odd = 1
+        multip_odd *= int(x[i])
+
+print ("Multiplication odd digit in %d - digit number = %d" % (count_digit, multip_odd))
