68508bfe05f0cdfe8f2d3bcaa76e653472afefa8
SuperLavrik <superlavrik@gmail.com>
2017-08-20 01:18:53 -0700

    remode code

--- task_30.py
+++ task_30.py
@@ -2,19 +2,14 @@ import string
 encryption_table = string.ascii_lowercase + string.digits + \
                    " " + string.ascii_uppercase + string.punctuation
 
-# print (encryption_table, len(encryption_table))
-
 user_str = input("Input you snring :  ")
 coded_str = ""
-for i in range(len(user_str)):
-    if user_str[i] in encryption_table:
-        for j in range(len(encryption_table)):
-            #print(user_str[i], encryption_table[j])
-            if user_str[i] == encryption_table[j]:
-                if j + 5 > len(encryption_table):
-                    j -= len(encryption_table)
-                #print(encryption_table[j + 5])
-                coded_str += encryption_table[j + 5]
+
+for i in user_str:
+    if i in encryption_table:
+        j = int(encryption_table.index(i))
+        coded_str += encryption_table[(j + 5) % \
+                     len(encryption_table)]
     else :
         print ("Simbol %s not detected" % user_str[i])
         print ("Try again")
